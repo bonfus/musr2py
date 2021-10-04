8366413e48c5b1e45dcb8362e0184562631c3d5f
Pietro Bonfa <bonfus@gmail.com>
2021-10-04 10:03:52 +0200

    Added parameters names

--- parse.py
+++ parse.py
@@ -1,8 +1,27 @@
+from string import Template
+
+# These are already dealt with by pybind11
 skip = [
         'MuSR_td_PSI_bin',
         '~MuSR_td_PSI_bin'
         ]
 
+# == Remove type info when collecting arguments names
+def replace_dict(where, what):
+    for word, rword in what.items():
+        where = where.replace(word, rword)
+    return where
+
+type_dict = {'int': '',
+             'char': '',
+             '*': '',
+             '&': '',
+             'double': '',
+             'const' : ''
+             }
+# ==
+
+code = ""
 with open('MuSR_td_PSI_bin.cpp','r') as f:
     while True:
         line = f.readline()
@@ -20,10 +39,27 @@ with open('MuSR_td_PSI_bin.cpp','r') as f:
                     name = function.split('(')[0].strip()
                     if name in skip:
                         break
+                    while not ( ')' in function):
+                        function += f.readline()
+                    params = function.split('(')[1].split(')')[0].split(',')
+                    params = [replace_dict(x.replace('\n',''), type_dict).strip() for x in params]
+                    params = [x for x in params if x!='']
+
+
+
                     description = description.replace('\n','\\n').replace('"','\\"').replace('\<','<').replace('\>','>')
-                    print("  .def(\"{name}\", &MuSR_td_PSI_bin::{name}, \"{desc}\")".format(name=name,desc=description))
+                    if params:
+                        params = ','.join(['py::arg("{}")'.format(x) for x in params]) + ','
+                    else:
+                        params = ''
+                    code += "  .def(\"{name}\", &MuSR_td_PSI_bin::{name}, {params} \"{desc}\")\n".format(name=name,params=params, desc=description)
 
                     break
                 else:
                     description += line
 
+
+with open('pybind.tmpl', 'r') as f:
+    src = Template(f.read())
+    result = src.substitute({'generatedcode':code})
+    print(result)
